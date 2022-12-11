de4541c442c8c4331d4b0e6f51989c0c5c6c1015
Ravanson <73492526+Ravanson@users.noreply.github.com>
2022-12-11 05:50:49 +0300

    Update

---
+++ FoCapp/MainMenu.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace FoCapp;
+
+public partial class MainMenu : UserControl
+{
+    public MainMenu()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
---
+++ FoCapp/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace FoCapp;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
